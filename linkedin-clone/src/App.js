994141047bd565cc2d80ac6ff7fe2b0fd155280a
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-10-22 02:12:36 +0530

    sidebar

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
       <Header />
       {/* App body */}
       <div className="app__body">
-        {/* <Sidebar /> */}
-        {/* Feed */}
+        <Sidebar />
+        {/* <Feed /> */}
         {/* Widgets */}
       </div>
     </div>
