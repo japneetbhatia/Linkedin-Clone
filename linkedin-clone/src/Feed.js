95d70aa3b177900ae46f2eb58ed67b8b3c93b9a9
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-10-22 20:03:52 +0530

    feed

---
+++ linkedin-clone/src/Feed.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import './Feed.css';
+import CreateIcon from "@mui/icons-material/Create";
+import InputOption from './InputOption';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import Post from './Post';
+
+function Feed() {
+    const [posts, setPosts] = useState([]);
+
+    const sendPost = e => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className="feed">
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text" />
+                        <button onClick={sendPost} type="submit">Send</button>
+                    </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
+                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E"/>
+                </div>
+            </div>
+
+            {/* Posts */}
+            {posts.map((post) => {
+                <Post />
+            })}
+            <Post name='Japneet Kaur' description='This is demo test' message='This works fine'/>
+        </div>
+    )
+}
+
+export default Feed
